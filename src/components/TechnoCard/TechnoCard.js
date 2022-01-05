7c609a45a437361de10ba4e4b107f953adb7dfe7
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2022-01-05 17:28:07 +0100

    responsive slides

--- src/components/TechnoCard/TechnoCard.js
+++ src/components/TechnoCard/TechnoCard.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Autoplay } from 'swiper'
 import 'swiper/css'
@@ -14,12 +14,27 @@ import {
 SwiperCore.use([Autoplay])
 
 export default function TechnoCard () {
+  let nbSlides = 3
+  if (window.innerWidth < 640) {
+    nbSlides = 1
+  }
+  const [slidesPerView, setSlides] = useState(nbSlides)
+
+  React.useEffect(() => {
+    function handleResize () {
+      window.innerWidth < 640 ? setSlides(1) : setSlides(3)
+    }
+    window.addEventListener('resize', handleResize)
+  })
+
+  React.useState()
+
   return (
     <div className='card grid'>
       <Swiper
         className='w-full h-full flex justify-center'
         centeredSlides
-        slidesPerView={3}
+        slidesPerView={slidesPerView}
         loop
         autoplay={{
           delay: 3500,
