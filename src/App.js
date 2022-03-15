13bc464dab7ddfbd3a42e8184728ce2487bb590f
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2022-03-15 13:09:57 +0100

    parallax

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Site from './containers/Site/Site'
+import { LocomotiveScrollProvider } from 'react-locomotive-scroll'
 
 function App () {
+  const containerRef = useRef(null)
+
   return (
-    <>
-      <Site />
-    </>
+    <LocomotiveScrollProvider
+      options={
+      {
+        smooth: true
+        // ... all available Locomotive Scroll instance options
+      }
+    }
+      watch={
+      [
+        // ..all the dependencies you want to watch to update the scroll.
+        //  Basicaly, you would want to watch page/location changes
+        //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+      ]
+    }
+      containerRef={containerRef}
+    >
+      <main data-scroll-container ref={containerRef}>
+        <Site />
+      </main>
+    </LocomotiveScrollProvider>
   )
 }
 
