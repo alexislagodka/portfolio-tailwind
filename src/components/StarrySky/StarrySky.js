779c2332c8333b7d0a58630d7aab76ade2e803df
Alexis LAGODKA <alexis.lagodka@bethunebruay.fr>
2022-07-12 16:00:51 +0200

    comment shootingstars

--- src/components/StarrySky/StarrySky.js
+++ src/components/StarrySky/StarrySky.js
@@ -65,7 +65,7 @@ export default class StarrySky extends React.Component {
 
     componentDidMount () {
       this.starryNight()
-      this.shootingStars()
+      // this.shootingStars()
     }
 
     render () {
